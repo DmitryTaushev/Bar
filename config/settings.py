eed547b0791c22981997ac877d038968cc9ec55b
TaushevDN <123morfix@gmail.com>
2025-06-25 11:28:44 +0500

    Прописаны настройки static и media

--- config/settings.py
+++ config/settings.py
@@ -13,14 +13,12 @@ import os
 
 from pathlib import Path
 
-
 from dotenv import load_dotenv
 
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -33,7 +31,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -45,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    #project apps
+    # project apps
     'users',
     'drinks',
     'dishes'
@@ -81,7 +78,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -94,19 +90,17 @@ DATABASE = os.getenv('MS_SQL_DATABASE')
 PAD_DATABASE = os.getenv('MS_PAD_DATABASE')
 DRIVER = os.getenv('MS_SQL_DRIVER')
 
-
 DATABASES = {
     'default': {
         'ENGINE': 'mssql',
         'NAME': DATABASE,
         'HOST': HOST,
         'PORT': '',
-        'OPTIONS':{
-            'driver':DRIVER
+        'OPTIONS': {
+            'driver': DRIVER
         }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -126,7 +120,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -138,12 +131,18 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = (
+        BASE_DIR / 'media'
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
