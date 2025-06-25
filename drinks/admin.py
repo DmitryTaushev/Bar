104cf813a9b48baff4b6258a5868c7aaecc9ea36
TaushevDN <123morfix@gmail.com>
2025-06-25 11:09:07 +0500

    Модели Type и Drink зарегистрированы в админке

--- drinks/admin.py
+++ drinks/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from drinks.models import Type, Drink
+
+@admin.register(Type)
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ('pk','name')
+    ordering = ('pk',)
+
+@admin.register(Drink)
+class DrinkAdmin(admin.ModelAdmin):
+    list_display = ('pk','name','alco',)
+    list_filter = ('alco',)
+    ordering = ('name',)
