4a9ace4ce2f4343910969ef175941decef9ad821
TaushevDN <123morfix@gmail.com>
2025-06-25 14:03:46 +0500

    1.Внесли колонку role в модель User 2.Провели миграции

--- users/management/commands/ccsu.py
+++ users/management/commands/ccsu.py
@@ -1,6 +1,6 @@
 from django.core.management import BaseCommand
 
-from users.models import User, UserRoles
+from users.models import User,UserRoles
 
 
 class Command(BaseCommand):
--- users/models.py
+++ users/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 NULLABLE = {'blank':True,'null':True}
+
+class UserRoles(models.TextChoices):
+    ADMIN = 'admin', _('admin')
+    MODERATOR = 'moderator', _('moderator')
+    USER = 'user', _('user')
 
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True,verbose_name='email')
+    role = models.CharField(max_length=9, choices=UserRoles.choices, default=UserRoles.USER)
     phone = models.CharField(unique = True,max_length=35,verbose_name='phone_number',**NULLABLE)
     telegram = models.CharField(max_length=150,unique=True,verbose_name='telegram_username',**NULLABLE)
     is_active = models.BooleanField(default=True, verbose_name='active')
