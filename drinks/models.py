4b522fb3f863796f394ff962c8ce331758c4760d
TaushevDN <123morfix@gmail.com>
2025-06-25 11:05:28 +0500

    1.Добавили модель Type,Drink 2.Проведена миграция

--- drinks/models.py
+++ drinks/models.py
@@ -1,11 +1,25 @@
 from django.db import models
+from users.models import NULLABLE
 
-class Name(models.Model):
-    name = models.CharField(max_length=100,verbose_name='name')
+class Type(models.Model):
+    name = models.CharField(max_length=100,verbose_name='alco')
     description = models.CharField(max_length=1000, verbose_name='description')
 
     def __str__(self):
         return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'alco'
+        verbose_name_plural = 'alcos'
+
+class Drink(models.Model):
+    name = models.CharField(max_length=250,verbose_name='name')
+    alco = models.ForeignKey(Type, on_delete=models.CASCADE,verbose_name='alco')
+    photo = models.ImageField(upload_to = 'drinks/',**NULLABLE,verbose_name = 'image')
+
+
+    def __str__(self):
+        return f'{self.name} ({self.alco})'
 
     class Meta:
         verbose_name = 'name'
