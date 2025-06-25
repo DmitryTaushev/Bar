d3293edff80a77e4d3c1f29737bf99548e7b5649
TaushevDN <123morfix@gmail.com>
2025-06-25 11:46:41 +0500

    1.Написан type_list_view 2.Написан urls для type_list_view 3.Добавлены пустые шаблоны types.html,drinks.html

--- drinks/views.py
+++ drinks/views.py
@@ -3,7 +3,23 @@ from django.shortcuts import render
 from drinks.models import Type, Drink
 def index_view(request):
     context = {
-        'objects_list':Type.objects.all()[:3],
+        'objects_list':Type.objects.all()[:2],
         'title': 'Крепкое - Хмельное'
     }
     return render(request, 'drinks/index.html',context = context)
+
+def type_list_view(request):
+    context = {
+        'objects_list':Type.objects.all(),
+        'title': 'Напитки'
+    }
+    return render(request,'drinks/types.html',context = context)
+
+def type_drink_list_view(request,pk:int):
+    type_object = Type.objects.get(pk=pk)
+    context = {
+        'objects_list': Drink.objects.filter(type_id = pk),
+        'title':f'Собаки породы - {type_object.name}',
+        'type_pk': type_object.pk
+    }
+    return render(request, 'drinks/drinks.html',context=context)
