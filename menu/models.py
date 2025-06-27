2475d6117f870311efd1b1e58f5081d797fb8f60
TaushevDN <123morfix@gmail.com>
2025-06-27 17:40:11 +0500

    Добавили price в модель DrinkAndDish

--- menu/models.py
+++ menu/models.py
@@ -18,7 +18,7 @@ class DrinkAndDish(models.Model):
     name = models.CharField(max_length=250, verbose_name='Название')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
     photo = models.ImageField(upload_to='drinks/', **NULLABLE, verbose_name='Фото')
-
+    price = models.IntegerField(default=0,verbose_name='Цена')
     def __str__(self):
         return f'{self.name} ({self.category})'
 
