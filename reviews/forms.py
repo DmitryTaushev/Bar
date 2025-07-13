250385c82074f99fc7bd413d3d9b7090ee795768
TaushevDN <123morfix@gmail.com>
2025-07-13 10:03:57 +0500

    Финальные правки

--- reviews/forms.py
+++ reviews/forms.py
@@ -4,7 +4,7 @@ from users.forms import StyleFormMixin
 
 class ReviewAdminForm(StyleFormMixin,forms.ModelForm):
     title = forms.CharField(max_length=150, label='Заголовок')
-    content = forms.SlugField(max_length=150,label='Содержимое')
+    content = forms.CharField(max_length=150,label='Содержимое')
     slug = forms.SlugField(max_length=20, initial='temp_slug', widget=forms.HiddenInput())
 
     class Meta:
